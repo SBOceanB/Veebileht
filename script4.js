8e9e0422d5a94b78bc0ae4469540bf8845b33e16
SBOceanB <helena.sinilaid@gmail.com>
2025-11-27 13:19:05 +0200

    Add comments to script4.js for clarity
    Added comments in Estonian to explain the slideshow functionality.

--- script4.js
+++ script4.js
@@ -1,4 +1,9 @@
-  const slides = [
+// Autor: Ayrton Rungi
+// Fail: script4.js
+// Funktsioon: Slideshow piltide vahetamine
+
+// Massiiv kõikide slaidide andmetega
+const slides = [
     {
       src: "https://imageproxy.wolt.com/menu/menu-images/64f725e81d2ba43a2e750196/1b4dff34-bf81-11ee-8442-96cc1f91893a_img_7751__1_.jpg?w=960",
       name: "Caesari wrap kanaga"
@@ -12,21 +17,22 @@
       name: "Jogurt puuviljadega"
     },
   ];
-
+// Hoiab aktiivse pildi indeksit
   let index = 0;
+// Võtame HTML elemendid kasutamiseks
   const slideImg = document.getElementById("slide-img");
   const slideName = document.getElementById("slide-name");
-
+// Funktsioon, mis uuendab ekraanil olevat pilti ja nime
   function updateSlide() {
     slideImg.src = slides[index].src;
     slideName.textContent = slides[index].name;
   }
-
+// Nupu "next" vajutus – liigu järgmisele pildile
   document.querySelector(".next").addEventListener("click", () => {
     index = (index + 1) % slides.length;
     updateSlide();
   });
-
+// Nupu "prev" vajutus – liigu eelmisele pildile
   document.querySelector(".prev").addEventListener("click", () => {
     index = (index - 1 + slides.length) % slides.length;
     updateSlide();
