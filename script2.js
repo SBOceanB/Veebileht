1c30b029b8d2b4e70cc24770407428a0ee1f80ec
SBOceanB <helena.sinilaid@gmail.com>
2025-11-27 13:19:17 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,9 @@
-  const slides = [
+// Autor: Ayrton Rungi
+// Fail: script2.js
+// Funktsioon: Slideshow piltide vahetamine
+
+// Massiiv kõikide slaidide andmetega
+const slides = [
     {
       src: "https://tartu.operapizza.ee/_next/image?url=https%3A%2F%2Fdb.operapizza.ee%2Fuploads%2Fpepperoni_a6a91812a3.jpg&w=1920&q=75",
       name: "Pepperoni"
@@ -16,21 +21,22 @@
       name: "Opera"
     }
   ];
-
+// Hoiab aktiivse pildi indeksit
   let index = 0;
+// Võtame HTML elemendid kasutamiseks
   const slideImg = document.getElementById("slide-img");
   const slideName = document.getElementById("slide-name");
-
+// Funktsioon, mis uuendab ekraanil olevat pilti ja nime
   function updateSlide() {
     slideImg.src = slides[index].src;
     slideName.textContent = slides[index].name;
   }
-
+// Nupu "next" vajutus – liigu järgmisele pildile
   document.querySelector(".next").addEventListener("click", () => {
     index = (index + 1) % slides.length;
     updateSlide();
   });
-
+// Nupu "prev" vajutus – liigu eelmisele pildile
   document.querySelector(".prev").addEventListener("click", () => {
     index = (index - 1 + slides.length) % slides.length;
     updateSlide();
